520d28c5a26df1dbea977d391ff928f16ef6b748
Moyosore Ogunleye <moyosore.ogunleye@gmail.com>
2021-11-04 15:20:06 +0100

    Contact Us component added

---
+++ src/components/contact.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const ContactMainDiv = styled.div`
+`
+const Contact = () => {
+    return (
+        <ContactMainDiv>
+            
+        </ContactMainDiv>
+    )
+}
+
+export default Contact
