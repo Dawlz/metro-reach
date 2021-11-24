85917eb6233f599129816e5ff02c9a2139b2b6b4
Moyosore Ogunleye <moyosore.ogunleye@gmail.com>
2021-11-24 13:13:29 +0100

    created blog component

---
+++ src/components/blog.js
@@ -0,0 +1,10 @@
+const Blog = () => {
+    return (
+        <div>
+            <h1>Our Articles and Publications</h1>
+            <p>Blog is coming soon</p>
+        </div>
+    )
+}
+
+export default Blog
